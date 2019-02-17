ce07552f4ff557fa43cfd7e2e777cde6a5e232c8
Unknown <santaclauusrs@gmail.com>
2019-02-17 21:54:50 +0000

    JordonTest
    Yeet

---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ForwardBackwardTranslation = Input.GetAxis("Vertical") * Speed;
+        float LeftRightTranslation = Input.GetAxis("Horizontal") * Speed;
+
+        transform.Translate(LeftRightTranslation, 0, ForwardBackwardTranslation);
+    }
+}
